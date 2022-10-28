a7e5e0fd7d5241cbe376e281f708dba658707881
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-10-28 12:27:49 +0100

    Add files via upload
    Added comments and shapes, blue square on a white circle on a grey canvas

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,18 @@
+//the setup function runs once at the start
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
 }
-
+//the draw function is a constant loop and runs 60 times a second
 function draw() {
-    background(220);
+    background(220); //greyscale colour for background
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(300, 300, 400);
+    
+    fill(167,217,212);
+    square (150,150,300)
+    
+  
 }
+
